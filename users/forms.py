8c57dfd68a736e90f9ea4eb924482574f454d00a
Zhilyaev <zhilyaev.ms74@gmail.com>
2024-02-01 23:33:43 +0300

    update users app

--- users/forms.py
+++ users/forms.py
@@ -2,7 +2,7 @@ from django import forms
 
 
 class LoginUserForm(forms.Form):
-    username = forms.CharField(label="Логин",
-                               widget=forms.TextInput(attrs={'class': 'form-input'}))
-    password = forms.CharField(label="Пароль",
-                               widget=forms.PasswordInput(attrs={'class': 'form-input'}))
+    username = forms.CharField(label="",
+                               widget=forms.TextInput(attrs={'class': 'css_input', 'placeholder': 'Имя пользователя'}))
+    password = forms.CharField(label="",
+                               widget=forms.PasswordInput(attrs={'class': 'css_input', 'placeholder': 'Пароль'}))
