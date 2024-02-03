e5733def3d000ec7204828552a14d51c249cbc8c
Maxim_Zhilyaev <Zhilyaev.ms74@gmail.com>
2024-02-03 17:38:24 +0300

    update authorization in users app

--- users/forms.py
+++ users/forms.py
@@ -1,8 +1,14 @@
 from django import forms
+from django.contrib.auth import get_user_model
+from django.contrib.auth.forms import AuthenticationForm
 
 
-class LoginUserForm(forms.Form):
+class LoginUserForm(AuthenticationForm):
     username = forms.CharField(label="",
                                widget=forms.TextInput(attrs={'class': 'css_input', 'placeholder': 'Имя пользователя'}))
     password = forms.CharField(label="",
                                widget=forms.PasswordInput(attrs={'class': 'css_input', 'placeholder': 'Пароль'}))
+
+    class Meta:
+        model = get_user_model()
+        fields = ['username', 'password']
--- users/views.py
+++ users/views.py
@@ -1,27 +1,14 @@
-from django.contrib.auth import authenticate, login
-from django.http import HttpResponse, HttpResponseRedirect
+from django.contrib.auth import authenticate, login, logout
+from django.contrib.auth.views import LoginView
+from django.http import HttpResponseRedirect
 from django.shortcuts import render
 from django.urls import reverse
 
 from .forms import LoginUserForm
 
 
-
-def login_user(request):
-    if request.method == 'POST':
-        form = LoginUserForm(request.POST)
-        if form.is_valid():
-            cd = form.cleaned_data
-            user = authenticate(request, username=cd['username'], password=cd['password'])
-            if user and user.is_active:
-                login(request, user)
-                return HttpResponseRedirect(reverse('home'))
-    else:
-        form = LoginUserForm()
-    return render(request, 'users/login.html', {'form': form})
-
-
-def logout_user(request):
-    return HttpResponse("Это страница Логаута")
-
+class LoginUser(LoginView):
+    form_class = LoginUserForm
+    template_name = 'users/login.html'
+    extra_context = {'title': 'Авторизация'}
 
