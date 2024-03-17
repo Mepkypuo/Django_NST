3c39a1b9771a162871247d029df5cf7030085cff
Zhilyaev <zhilyaev.ms74@gmail.com>
2024-03-18 00:25:33 +0300

    обновил view.py в турнире

--- tournament/views.py
+++ tournament/views.py
@@ -3,96 +3,88 @@ from django.http import HttpResponseForbidden, HttpResponseRedirect
 from django.urls import reverse
 from django.utils import timezone
 from django.views.generic import TemplateView
+from django.contrib import messages
+from django.db.models import Prefetch
 
 from .forms import AnswerForm
 from .models import Tournament, Participant, Question, Answer
 
-
 class TournamentView(TemplateView):
     template_name = 'tournament/index.html'
 
-    def get(self, request, tournament_id):
+    def _get_base_context(self, tournament_id, selected_question=None):
         tournament = get_object_or_404(Tournament, pk=tournament_id)
-        current_time = timezone.now()
-
-        # Проверка доступа к турниру для зарегистрированных пользователей
-        if not request.user.is_authenticated:
-            return HttpResponseForbidden("Для доступа к турниру необходимо войти в систему.")
-
-        # Проверка доступа к турниру для участников с учетом времени начала и окончания
-        if current_time < tournament.start_time or current_time > tournament.end_time:
-            return HttpResponseForbidden("Доступ к турниру закрыт в данный момент.")
-
-        # Проверка доступа к турниру для пользователей, участвующих в нем
-        participant = Participant.objects.filter(user=request.user, tournament=tournament).first()
-        if not participant:
-            return HttpResponseForbidden("Вы не участвуете в данном турнире.")
-
-        # Получение списка вопросов для турнира
-        questions = Question.objects.filter(tournament=tournament)
+        participant = get_object_or_404(Participant, user=self.request.user, tournament=tournament)
+        questions = Question.objects.filter(tournament=tournament).prefetch_related(
+            Prefetch('answer_set', queryset=Answer.objects.filter(participant=participant), to_attr='participant_answers')
+        )
 
         context = {
             'title': "Турнир",
             'tournament': tournament,
             'participant': participant,
             'questions': questions,
-            'selected_question': None,
-            'answer_form': AnswerForm()
+            'selected_question': selected_question,
+            'answer_form': AnswerForm(),
         }
 
+        return context
+
+    def get(self, request, tournament_id):
+        tournament = get_object_or_404(Tournament, pk=tournament_id)
+        current_time = timezone.now()
+
+        if not request.user.is_authenticated:
+            return HttpResponseForbidden("Для доступа к турниру необходимо войти в систему.")
+
+        if current_time < tournament.start_time or current_time > tournament.end_time:
+            return HttpResponseForbidden("Доступ к турниру закрыт в данный момент.")
+
+        selected_question_id = request.GET.get('selected_question')
+        selected_question = None
+        if selected_question_id:
+            selected_question = get_object_or_404(Question, pk=selected_question_id, tournament=tournament)
+
+        context = self._get_base_context(tournament_id, selected_question=selected_question)
         return self.render_to_response(context)
 
     def post(self, request, tournament_id):
-        tournament = get_object_or_404(Tournament, pk=tournament_id)
+        form_type = request.POST.get('form_type')
 
-        form_type = request.POST.get('form_type', None)
-
-        # POST запрос для выбора вопроса
         if form_type == 'choose_question':
             question_id = request.POST.get('question_id')
-            selected_question = get_object_or_404(Question, pk=question_id)
+            try:
+                selected_question = Question.objects.get(pk=question_id, tournament_id=tournament_id)
+            except Question.DoesNotExist:
+                messages.error(request, "Вопрос не найден.")
+                return redirect('tournament:tournament', tournament_id=tournament_id)
 
-            context = {
-                'title': "Турнир",
-                'tournament': tournament,
-                'participant': Participant.objects.get(user=request.user, tournament=tournament),
-                'questions': Question.objects.filter(tournament=tournament),
-                'selected_question': selected_question,
-                'answer_form': AnswerForm(),  # Добавляем форму ответа в контекст
-            }
-            return self.render_to_response(context)
-
-        # POST запрос для отправки ответа
         elif form_type == 'submit_answer':
             form = AnswerForm(request.POST)
-
             if form.is_valid():
-                answer = form.save(commit=False)
-                answer.participant = Participant.objects.get(user=request.user, tournament=tournament)
-                answer.question = Question.objects.get(pk=request.POST['question_id'])
-                answer.save()
+                try:
+                    answer = form.save(commit=False)
+                    answer.participant = Participant.objects.get(user=request.user, tournament_id=tournament_id)
+                    answer.question = get_object_or_404(Question, pk=request.POST['question_id'],
+                                                        tournament_id=tournament_id)
+                    answer.save()
+                    messages.success(request, "Ваш ответ сохранен.")
+                    return HttpResponseRedirect(reverse('tournament:tournament', args=(
+                    tournament_id,)) + f"?selected_question={answer.question.pk}")
+                except (Participant.DoesNotExist, Question.DoesNotExist):
+                    messages.error(request, "Произошла ошибка при сохранении ответа.")
 
-                # Получаем выбранный вопрос из запроса POST
-                selected_question_id = request.POST.get('question_id')
-                selected_question = get_object_or_404(Question, pk=selected_question_id)
+            # Если форма не валидна, перенаправляем обратно на страницу турнира
+            return redirect('tournament:tournament', tournament_id=tournament_id)
 
-                # Получаем турнир из контекста или из базы данных
-                tournament = get_object_or_404(Tournament, pk=tournament_id)
+        else:
+            messages.error(request, "Неверный запрос.")
+            return redirect('tournament:tournament', tournament_id=tournament_id)
 
-                # Получаем участника из контекста или из базы данных
-                participant = Participant.objects.get(user=request.user, tournament=tournament)
+        # Если выбран вопрос, возвращаем пользователя на страницу турнира с выбранным вопросом
+        if 'selected_question' in locals():
+            return HttpResponseRedirect(
+                reverse('tournament:tournament', args=(tournament_id,)) + f"?selected_question={selected_question.pk}")
 
-                context = {
-                    'title': "Турнир",
-                    'tournament': tournament,
-                    'participant': participant,
-                    'questions': Question.objects.filter(tournament=tournament),
-                    'selected_question': selected_question,
-                    'answer_form': AnswerForm(),
-                }
-
-                return self.render_to_response(context)
-
-        # Если не удалось обработать POST запрос, возвращаем 404
-        return HttpResponseForbidden("Invalid POST request.")
-
+        # Если не было действий связанных с выбором вопроса, просто вернуть пользователя на страницу турнира
+        return redirect('tournament:tournament', tournament_id=tournament_id)
